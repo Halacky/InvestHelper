a17575e2078f132211c269edc8867b2906730e4d
ialakey <prosoulk2017@gmail.com>
2021-10-09 05:53:28 +0800

    окна авторизации

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './screens/WelcomeScreen';
 import BlankScreen from './screens/BlankScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import ForgotpasswordScreen from './screens/ForgotpasswordScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,9 +14,12 @@ export default function App() {
   return (
   <NavigationContainer>
     <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="SignupScreen" component={SignupScreen} />
+      <Stack.Screen name="ForgotpasswordScreen" component={ForgotpasswordScreen} />
       <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
       <Stack.Screen name="BlankScreen" component={BlankScreen} />
-    </Stack.Navigator>
+         </Stack.Navigator>
   </NavigationContainer>
   );
 }
---
+++ screens/SignupScreen.js
@@ -0,0 +1,92 @@
+import React, {useContext, useState} from 'react';
+import {View, Text, TouchableOpacity, Platform, StyleSheet} from 'react-native';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+
+const SignupScreen = ({navigation}) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Создать аккаунт</Text>
+
+      <FormInput
+        labelValue={email}
+        onChangeText={(userEmail) => setEmail(userEmail)}
+        placeholderText="Email"
+        iconType="user"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+
+      <FormInput
+        labelValue={password}
+        onChangeText={(userPassword) => setPassword(userPassword)}
+        placeholderText="Пароль"
+        iconType="lock"
+        secureTextEntry={true}
+      />
+
+      <FormInput
+        labelValue={confirmPassword}
+        onChangeText={(userPassword) => setConfirmPassword(userPassword)}
+        placeholderText="Подтвердите пароль"
+        iconType="lock"
+        secureTextEntry={true}
+      />
+
+      <FormButton
+        buttonTitle="Зарегистрироваться"
+        onPress={() => register(email, password)}
+      />
+
+      <TouchableOpacity
+        style={styles.navButton}
+        onPress={() => navigation.navigate('LoginScreen')}>
+        <Text style={styles.navButtonText}>У вас уже есть аккаунт? Войдите здесь</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SignupScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f9fafd',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    fontFamily: 'Kufam-SemiBoldItalic',
+    fontSize: 28,
+    marginBottom: 10,
+    color: '#051d5f',
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#2e64e5',
+    fontFamily: 'Lato-Regular',
+  },
+  textPrivate: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical: 35,
+    justifyContent: 'center',
+  },
+  color_textPrivate: {
+    fontSize: 13,
+    fontWeight: '400',
+    fontFamily: 'Lato-Regular',
+    color: 'grey',
+  },
+});
